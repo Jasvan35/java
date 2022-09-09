06200e95f7aad5e0b1837e89b18a14844c384046
Jasvan35 <113084005+Jasvan35@users.noreply.github.com>
2022-09-09 23:11:25 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,22 @@
+// Practical 1 Student Details 
+import java.util.Scanner;
+class Student
+{
+	public static void main(String[] args)
+	{
+		int rollno;//assuming roll no. as integer 
+		String name;//assuming Student name as string
+		double attendance;//assuming attendance percentage as a double
+		System.out.println("Enter student details:");//printing statement 
+		Scanner sc=new Scanner(System.in);//create a Scanner object
+		System.out.println("Enter The RollNO.:");//printing statement
+		rollno=sc.nextInt();//Read user input
+		System.out.println("Enter the Name of Student:");//printing statement
+		name=sc.next();//Read user input
+		System.out.println("Enter the Percentage of Attendance:");//printing statement
+		attendance=sc.nextDouble();//Read user input
+		System.out.println("The Roll NO. is :"  +rollno);//printing statement
+		System.out.println("Name of the Student is :"  +name);//printing statement
+		System.out.println("Attendance Percentage is :" +attendance);//printing statement
+	}
+}
