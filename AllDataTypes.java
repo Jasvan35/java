566317fefd2f647d9cfe347a6a601154a78667f0
Jasvan35 <113084005+Jasvan35@users.noreply.github.com>
2022-09-26 02:48:24 +0530

    Add files via upload

---
+++ AllDataTypes.java
@@ -0,0 +1,29 @@
+public class AllDataTypes {
+    public static void main(String args[]) {
+        
+      boolean Java = true; // Boolean Datatype Assign 
+      System.out.println(Java);   // Output will be true
+      
+      byte n = 127;  // Byte Datatype Assign
+      System.out.println(n); // prints 127
+      
+      char  alpha = 'J'; // Character Datatype Assign 
+      System.out.println(alpha); // prints J
+      
+      short p = 3435;  // Short Datatype Assign
+      System.out.println(p); // prints the value present in n i.e. 3435
+     
+      int num = 5464564;  // Integer Datatype Assign
+      System.out.println(num); // prints 5464564
+      
+      long num1 = 15000000000L; // Long Datatype Assign
+      System.out.println(num1); // prints 15000000000
+ 
+      double num3 = 79.678d; // Double Datatype Assign
+      System.out.println(num3); // prints double value
+    
+    String S1 = "Java String Data type"; // create strings
+    System.out.println(S1);  // print strings
+
+    }
+}
