d933a8c78b191b7b6f99825862a82a2c008862e8
Jasvan35 <113084005+Jasvan35@users.noreply.github.com>
2022-10-21 10:35:25 +0530

    Update LargestNoOfThree.java

--- LargestNoOfThree.java
+++ LargestNoOfThree.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 public class LargestNoOfThree{
 public static void main(String [ ] args){
-Scanner sc = new Scanner (System.in);
+Scanner sc = new Scanner (System.in);//Scanner class object creation
 int x,y,z;
 System.out.println("Enter A value:");
-x = sc.nextInt();
+x = sc.nextInt();//Reading input from user
 System.out.println("Enter B value:");
-y = sc.nextInt();
+y = sc.nextInt();//Reading input from user
 System.out.println("Enter C value:");
-z = sc.nextInt();
-if(x>y && x>z){
-System.out.println("A is Largest Number");
+z = sc.nextInt();//Reading input from user
+if(x>y && x>z)//Condition to check the A value is greater or Not
+{
+System.out.println("A is Largest Number");//Printing the largest number 
 }
-else if(y>x && y>z){
-System.out.println("B is Largest Number");
+else if(y>x && y>z)//Condition to check the B value is greater or Not
+{
+System.out.println("B is Largest Number");//Printing the largest number
 }
-else{
-System.out.println("C is Largest Number");
+else//If both condition are false then it will show C value is greater
+{
+System.out.println("C is Largest Number");//Printing the largest number
 }
 }
 }
